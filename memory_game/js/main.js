b847d246141ab1bb24c1b455837358617f197ab4
Hunter Maddux <phmaddux@gmail.com>
2017-08-21 20:01:35 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,26 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ['queen', 'queen', 'king', 'king'];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cardsInPlay = [];
+
+var cardOne = cards[1];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+	console.log("Y");
+	if (cards[1] === cards[2]) {
+		console.log("You found a match!");
+		alert("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
+	}
+} 
+
+
