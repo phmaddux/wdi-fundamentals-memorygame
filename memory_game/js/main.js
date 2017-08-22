2e11f0904b014a167c22a96ca44f3160ffe177ee
Hunter Maddux <phmaddux@gmail.com>
2017-08-22 18:30:54 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png",
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage:"images/queen-of-diamonds.png",
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png",
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 var cardsInPlay = [];
 var checkForMatch = function (){
   if (cardsInPlay.length === 2) {
@@ -11,8 +32,10 @@ var checkForMatch = function (){
 }
 var flipCard = function(cardId) {	
 	var card = cards[cardId]
-	cardsInPlay.push(card)
-	console.log("User flipped " + cards[cardId])
+	cardsInPlay.push(card.rank)
+	console.log("User flipped " + cards[cardId].rank)
+	console.log(card.cardImage)
+	console.log(card.suit)
 	checkForMatch();
 }
 flipCard(0)
