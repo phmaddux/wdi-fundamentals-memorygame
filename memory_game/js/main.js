707e3367540b0e74ea76e4b27dd83d2153b34b2e
Hunter Maddux <phmaddux@gmail.com>
2017-08-22 23:52:24 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -30,17 +30,30 @@ var checkForMatch = function (){
 	  }
 	}
 }
-var flipCard = function(cardId) {	
+var flipCard = function(cardId) {
 	var card = cards[cardId]
 	cardsInPlay.push(card.rank)
 	console.log("User flipped " + cards[cardId].rank)
 	console.log(card.cardImage)
 	console.log(card.suit)
+	document.getElementById('card' + cardId).setAttribute('src', card.cardImage)
 	checkForMatch();
 }
-flipCard(0)
-flipCard(2)
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.setAttribute('id', 'card' +i);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+	document.getElementById('card0').addEventListener('click', () => flipCard(0))
+	document.getElementById('card1').addEventListener('click', () => flipCard(1))
+	document.getElementById('card2').addEventListener('click', () => flipCard(2))
+	document.getElementById('card3').addEventListener('click', () => flipCard(3))
+}
 
+createBoard();
 
 
 
